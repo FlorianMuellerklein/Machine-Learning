48ecad3957d348b9616278ccf272f8a04543eef4
Florian Muellerklein <f.muellerklein@gmail.com>
2015-01-29 14:49:28 -0500

    tweaked logit use heart data for binary classification

--- LinReg_test.py
+++ LinReg_test.py
@@ -2,15 +2,15 @@ import numpy as np
 from LogisticClassifier import Logit
 
 # initialize linear regression parameters
-iterations = 200000
+iterations = 5
 alpha = 0.001
 
 # plot the data with seaborn (add this later)
 
-lgit = Logit(alpha = alpha, iterations = iterations, verbose = True)
+lgit = Logit(alpha = alpha, iterations = iterations, verbose = False)
 
 # load the example data stolen from 'http://aimotion.blogspot.com/2011/10/machine-learning-with-python-linear.html'
-data = np.loadtxt('Data/admission.csv', delimiter = ',')
+data = np.loadtxt('Data/heart.txt', delimiter = ',')
 X = data[:, 1:]
 y = data[:, 0]
 
@@ -20,12 +20,20 @@ X = lgit.transform(X)
 # fit the linear reg
 lgit.gradient_descent(X = X, y = y)
 
+# load testing dataset
+test = np.loadtxt('Data/heart_test.txt', delimiter = ',')
+X_test = test[:, 1:]
+y_test = test[:, 0]
+
+# transform testing data
+X_test = lgit.transform(X_test)
+
 # make a predictions
-prediction = np.zeros(shape = (y.size, 2))
+prediction = np.zeros(shape = (y_test.size, 2))
 correct = 0
-for i in range(y.size):
-    prediction[i,0] = lgit.predict(X[i, :], labels = True)
-    prediction[i, 1] = y[i]
+for i in range(y_test.size):
+    prediction[i,0] = lgit.predict(X_test[i, :], labels = True)
+    prediction[i, 1] = y_test[i]
     if prediction[i, 0] == prediction[i, 1]:
         correct = correct + 1
 
