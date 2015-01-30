6985c07c9bf5c046b70fdfac957a5ce9f305144e
Florian Muellerklein <f.muellerklein@gmail.com>
2015-01-30 11:59:06 -0500

    added sigmoid function to perceptron

--- ffPerceptron.py
+++ ffPerceptron.py
@@ -12,15 +12,24 @@ class Perceptron(object):
         # initialize weights
         self.w = None
 
+    def sigmoid(self, x):
+        """
+        Typical sigmoid function created from input vector x
+        :param x: input vector
+        :return: sigmoided vector
+        """
+        # typical sigmoid py line, seems to get errors with arrays
+        return 1 / (1 + np.exp(-x))
+
     def response(self, X):
         '''
         perceptron response
         :param X: input vector
         :return: perceptron out
         '''
-        y = np.dot(X, self.w)
+        y = self.sigmoid(np.dot(X, self.w))
         # in this super simple example perceptron will return 1 for positive guess and -1 for neg
-        if y >= 0:
+        if y >= 0.5:
             return 1
         else:
             return 0
@@ -42,11 +51,18 @@ class Perceptron(object):
         :return: updated parameters
         '''
         num_examples, num_features = np.shape(X)
+
+        # set up bias
+        bias = np.ones(shape =(num_examples,1))
+        X = np.hstack((bias, X))
+        print X[1]
+
         # initialize weight vector
-        self.w = np.random.rand(num_features)
+        self.w = np.random.rand(num_features + 1)
+        print 'starting weights:', self.w
 
         for i in range(self.iterations):
-            for j in range(X.shape[0]):
+            for j in range(num_examples):
                 prediction = self.response(X[j])
                 if prediction != y[j]:
                     error = y[j] - prediction
